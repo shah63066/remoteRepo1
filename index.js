20c837142a98b7d6ed79b60e62798b7144b5df9c
shah63066 <shahnawaazahamad71@gmail.com>
2025-05-28 14:51:50 +0530

    ncsdnnsddc
    asasaddsd

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware to serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Example route
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Home Page' });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
